4b709b4d583b995bd42db03b243a795a03d1467a
Ariana Tlacuatl <ariana.tlacuatl@gmail.com>
2021-03-22 20:38:41 -0600

    New project with challenge

---
+++ Section4/ChallengeAccepted/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main()
+{
+	std::string flower;
+	std::cout<<"Tell me your favorite flower: ";
+	std::cin>>flower;
+	std::cout<<"Amazing that's my favorite flower too"<<std::endl;
+	std::cout<<"No, really! "<<flower<<" is my favorite flower too!"<<std::endl;
+	
+	return 0;
+}
